3f70b1772825940c50b2b6af9e1c0cbe702e8d37
juristoeckli <juri.burg@gmail.com>
2021-03-19 01:43:33 +0000

    renamed wav_play

--- testing/audio_1/disable_onboar_audio.sh
+++
@@ -1,5 +0,0 @@
-echo "blacklist snd_bcm2835" >> /etc/modprobe.d/alsa-blacklist.conf
-
-echo "Config changed"
-echo "Open /lib/modprobe.d/aliases.conf and comment out the line options snd-usb-audio index=-2"
-echo "you nedd to Reboot the machine for the change to take effect!!"
--- testing/audio_1/install.sh
+++
@@ -1,6 +0,0 @@
-apt-get update
-apt-get install alsa-base alsa-utils
-alsamixer
-
-npm i node-aplay
-
